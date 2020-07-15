aff2e5fc37d7c1800fb27a40a5e2da9ad609ecf2
Ryan Greenup <exogenesis@protonmail.com>
2020-07-15 23:09:46 +1000

    Updated Help

--- bin/NoteFind.sh
+++ bin/NoteFind.sh
@@ -76,9 +76,6 @@ Help () {
 
     echo -e "    \e[3m\e[1m• Compatability \e[0m "
     echo
-    echo -e "        Match highlighting occurs automatically if \e[1m\$SHELL\e[0m is \e[1m **/fish\e[0m"
-    echo -e "        This uses \e[1mtmpfs\e[0m at \e[1m /dev/shm\e[0m, this should work on \e[3mArch\e[0m, \e[3mFedora\e[0m and \e[3mUbuntu\e[0m, I don't know about  \e[3mMacOS\e[0m "
-    echo
 }
 
 
