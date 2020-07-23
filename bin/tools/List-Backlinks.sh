6905388d27ad63efa1d715d6e77fc07b74f8060c
ryangreenup <exogenesis@protonmail.com>
2020-07-24 01:21:20 +1000

    [FIX] List BackLinks now takes STDIN/STDOUT #1
    This means that the xclip used will be governed by cadmus making it more
    cross platform

--- bin/tools/List-Backlinks.sh
+++ bin/tools/List-Backlinks.sh
@@ -6,8 +6,8 @@ command -v sd >/dev/null 2>&1 || { echo >&2 "I require sd (sed replacement) but 
 command -v xclip >/dev/null 2>&1 || { echo >&2 "I require xclip but it's not installed.  Aborting."; exit 1; }
 
 
+term=$(basename "${1}" | cut -f 1 -d '.')
 
-term=$(xclip -selection clipboard -o | xargs basename |  cut -f 1 -d '.')
 rg -e "\[.*\]\(.*$term\.md\)" -e "\[\[$term\]\]" -e "\[\[$term.*\]\]" \
     ~/Notes/MD/notes \
     -t markdown -ol
