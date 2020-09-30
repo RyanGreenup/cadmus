ffa1640104ce9c097d1233a7d5745cd95fb9f7fb
Ryan Greenup <exogenesis@protonmail.com>
2020-10-01 09:12:25 +1000

    Changed Note Search to MDCat from Bat
    (for aesthetics)

--- bin/NoteRecollSearch.sh
+++ bin/NoteRecollSearch.sh
@@ -121,10 +121,7 @@ NoteSearchRecoll () {
                 cut -c 8- | sed s/^/realpath\ \"/                     |\
                 sed s+\$+\"\ --relative-to\ \"./\"+ | bash'            \
             --bind pgup:preview-page-up,pgdn:preview-page-down    \
-            --preview "bat --color=always --line-range :500           \
-                    --terminal-width 80 --theme=TwoDark {+}           \
-                    --italic-text=always                              \
-                    --decorations=always"                             \
+            --preview "mdcat {+}"        \
             --color=fg:#f8f8f2,bg:-1,matched:#6272a4,current_fg:#50fa7b,current_bg:#381070,border:#ff79c6,prompt:#bd93f9,query:#bd93f9,marker:#f1fa8c,header:#f1fa8c
     }
     RELATIVE_PATH="$(RelativePath)"
